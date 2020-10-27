61769aaa3b20c0cba5947025e694811587d0ae14
Ana Nikolova <ani.nik19@gmail.com>
2020-10-27 07:41:02 +0100

    ghost appeared on the screen

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { LEVEL, OBJECT_TYPE } from "./setup";
+import { randomMovement } from "./ghostmoves";
 
 //Classes
 import GameBoard from "./GameBoard";
 import Pacman from "./Packman";
+import Ghost from "./Ghost";
 
 // DOM Elements
 const gameGrid = document.querySelector("#game");
@@ -26,9 +28,13 @@ function gameOver(pacman, grid) {}
 
 function checkCollision(pacman, ghost) {}
 
-function gameLoop(pacman, ghost) {
+function gameLoop(pacman, ghosts) {
   //console.log("works");
+  // Move Pacman
   gameBoard.moveCharacter(pacman);
+
+  // 3. Move ghosts
+  ghosts.forEach((ghost) => gameBoard.moveCharacter(ghost));
 }
 
 function startGame() {
@@ -45,8 +51,15 @@ function startGame() {
     pacman.handleKeyInput(e, gameBoard.objectExist)
   );
 
+  const ghosts = [
+    new Ghost(5, 188, randomMovement, OBJECT_TYPE.BLINKY),
+    new Ghost(4, 209, randomMovement, OBJECT_TYPE.PINKY),
+    new Ghost(3, 230, randomMovement, OBJECT_TYPE.INKY),
+    new Ghost(2, 251, randomMovement, OBJECT_TYPE.CLYDE),
+  ];
+
   // Gameloop
-  timer = setInterval(() => gameLoop(pacman), GLOBAL_SPEED);
+  timer = setInterval(() => gameLoop(pacman, ghosts), GLOBAL_SPEED);
 }
 
 // Initailize game
